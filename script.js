f8f602685a22950f25b30e38eeb32a1d999a8724
Lewyyshub <salamashvili21@gmail.com>
2024-11-17 21:45:46 +0400

    lecture 26 homework

---
+++ script.js
@@ -0,0 +1,17 @@
+let number = 1;
+
+async function addTitleAndPrice() {
+  try {
+    let response = await fetch(`https://fakestoreapi.com/products/${number}`);
+    let data = await response.json();
+    let p = document.createElement("p");
+    p.textContent = `სახელი ${data.title}, ფასი ${data.price}`;
+    document.body.appendChild(p);
+    number++;
+  } catch (error) {
+    let p1 = document.createElement("p");
+    p1.textContent = "შენგან პროგრამისტი არ გამოვა";
+    document.body.appendChild(p1);
+  }
+}
+document.getElementById("btn").addEventListener("click", addTitleAndPrice);
